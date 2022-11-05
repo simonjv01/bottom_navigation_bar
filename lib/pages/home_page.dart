d98329a00055063ab90ee4e8bc7340ba0217a6cc
simonjv01 <sjvargas01@gmail.com>
2022-11-05 19:41:57 -0400

    updates to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:bottom_navigation_bar/pages/account.dart';
+import 'package:bottom_navigation_bar/pages/blogs_page.dart';
+import 'package:bottom_navigation_bar/pages/favorites_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,9 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final PageController _pageController = PageController();
+  final List<Widget> _screens = [
+    const BlogsPage(), const FavoritesPage(), const AccountPage()
+  ];
+  void _onPageChanged(int index) {
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
+        children: _screens,
+      ),
       bottomNavigationBar: BottomNavigationBar(items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: ('Home')),
         BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ('Favorites')),
